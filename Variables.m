8ba2b386658e91aac1ed7a2e8b947dadfbce1bfe
gio-lp <121116352+gio-lp@users.noreply.github.com>
2024-03-13 18:37:48 +0100

    Create Variables.m

---
+++ Variables.m
@@ -0,0 +1,10 @@
+%Variables%
+%Exercise of Chapter 5 from Choen's book%
+
+% creating and destroying variable
+% to create a variable that store the  numer 7
+asdf=7
+% To clear a variable from MATLAB’s buffer
+clear asdf
+
+ % You can also type clear as* to clear all variables that start with as
